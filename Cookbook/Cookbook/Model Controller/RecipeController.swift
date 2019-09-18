01d89812fe9a2c5a82c58a139887dd3898b8b192
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-17 18:56:39 -0600

    Setup model controller

---
+++ Cookbook/Cookbook/Model Controller/RecipeController.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeController.swift
+//  Cookbook
+//
+//  Created by Mitchell Budge on 9/17/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+class RecipeController {
+    
+    // MARK: - Methods
+    
+    func createRecipe(title: String, cuisine: String, directions: String) -> Recipe {
+        let recipe = Recipe(title: title, cuisine: cuisine, directions: directions, context: CoreDataStack.shared.mainContext)
+        CoreDataStack.shared.saveToPersistentStore()
+        return recipe
+    }
+    
+    func deleteRecipe(recipe: Recipe) {
+        CoreDataStack.shared.mainContext.delete(recipe)
+        CoreDataStack.shared.saveToPersistentStore()
+    }
+    
+}
